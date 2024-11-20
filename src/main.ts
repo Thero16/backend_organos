030ded6fb24d33edb7e8e66cf191a8c37fa764e5
Thero16 <juan_pablo9687@hotmail.com>
2024-11-20 12:28:32 -0500

    vercel

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+
+let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.enableCors();
 
-  // Configuración global de validación
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true, // Transforma las propiedades de las solicitudes según los DTOs
-    }),
-  );
-
-  // Configuración de Swagger
-  const config = new DocumentBuilder()
-    .setTitle('API de Ejemplo')
-    .setDescription('Documentación de la API de ejemplo')
-    .setVersion('1.0')
-    .addTag('usuarios')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  // Configuración de CORS nativa de NestJS
-  app.enableCors({
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  });
-
-  // Escuchar en el puerto definido o en 3000 por defecto
-  await app.listen(process.env.PORT || 3000);
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  server = serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  if (!server) {
+    await bootstrap();
+  }
+  return server(event, context, callback);
+};
